065379998422be64d89a86b8f2442ff474c19841
Matija Brnas <mb8828@rit.edu>
2023-05-13 15:18:24 +0200

    user auth/registration

--- mebexBank.sql
+++ mebexBank.sql
@@ -1,24 +1,40 @@
+DROP DATABASE IF EXISTS mebex_bank;
 CREATE DATABASE mebex_bank;
 USE mebex_bank;
 
 CREATE TABLE customers(
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
     phone_number VARCHAR(30) NOT NULL,
     date_of_birth VARCHAR(20) NOT NULL,
-    cust_address VARCHAR(50) NOT NULL,
-    constraint PRIMARY KEY (id)
+    cust_address VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE customers_iban(
-	customer_id int,
-    constraint id_fk foreign key (id) references customers(id),
+    customer_id int,
+    constraint id_fk foreign key (customer_id) references customers(id),
     iban VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE user_authentication(
+	id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+	constraint primary key (id)
+);
 
-TRUNCATE TABLE customers;
-truncate table customers_iban;
+CREATE TABLE user_registration(
+	id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
 SELECT * FROM customers_iban;
 SELECT * FROM customers;
+SELECT * FROM user_authentication;
+SELECT * FROM user_registration;
+
